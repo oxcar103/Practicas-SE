25dbb1a020030e89d3cd81bd3f50f9d544bf5a58
oxcar103 <oxcar103@gmail.com>
2017-10-25 10:45:13 +0200

    Cambio de registros En teoría, las funciones sólo deben usar r0-r3

--- Práctica 3/buttons.s
+++ Práctica 3/buttons.s
@@ -98,24 +98,24 @@ pause:
 gpio_init:
         @ No necesitamos configurar GPIO22, GPIO23, GPIO26 y GPIO26 ya que usaremos sus valores por defecto
         @ Dirección del registro GPIO_DATA_SET0
-        ldr     r7, =GPIO_DATA_SET0
+        ldr     r2, =GPIO_DATA_SET0
 
         @ Establecemos a 1 la salida de los GPIO22 y GPIO23
-        ldr     r4, =BUTTON_S2_OUT
-        ldr     r5, [r4]
-        ldr     r4, =BUTTON_S3_OUT
-        ldr     r6, [r4]
-        orr     r5, r5, r6
-        str     r5, [r7]
+        ldr     r3, =BUTTON_S2_OUT
+        ldr     r0, [r3]
+        ldr     r3, =BUTTON_S3_OUT
+        ldr     r1, [r3]
+        orr     r0, r0, r1
+        str     r0, [r2]
 
         @ Configuramos GPIO44 y GPIO45 como salida
-        ldr     r7, =GPIO_PAD_DIR1
-        ldr     r4, =LED_RED_MASK
-        ldr     r5, [r4]
-        ldr     r4, =LED_GREEN_MASK
-        ldr     r6, [r4]
-        orr     r5, r5, r6
-        str     r5, [r7]
+        ldr     r2, =GPIO_PAD_DIR1
+        ldr     r3, =LED_RED_MASK
+        ldr     r0, [r3]
+        ldr     r3, =LED_GREEN_MASK
+        ldr     r1, [r3]
+        orr     r0, r0, r1
+        str     r0, [r2]
 
         @ Retornamos a donde se invocó la función
         mov     pc, lr
@@ -127,8 +127,8 @@ gpio_init:
 test_buttons:
         @ Comprobamos si el botón está pulsado
         ldr     r0, [r7]
-        ldr     r4, =CHECK_BUTTON
-        ldr     r1, [r4]
+        ldr     r3, =CHECK_BUTTON
+        ldr     r1, [r3]
         tst     r0, r1
 
         @ Si lo está, cambiamos el led que debemos encender y el botón que debemos comprobar en otra función
@@ -143,30 +143,30 @@ test_buttons:
         .type   pressed_button, %function
 pressed_button:
         @ Sumamos ambas máscaras
-        ldr     r4, =BUTTON_S2_IN
-        ldr     r5, [r4]
-        ldr     r4, =BUTTON_S3_IN
-        ldr     r6, [r4]
-        orr     r5, r5, r6
+        ldr     r3, =BUTTON_S2_IN
+        ldr     r0, [r3]
+        ldr     r3, =BUTTON_S3_IN
+        ldr     r1, [r3]
+        orr     r0, r0, r1
 
         @ Permutamos el valor de CHECK_BUTTON
-        ldr     r4, =CHECK_BUTTON
-        ldr     r6, [r4]
-        eor     r5, r5, r6
-        str     r5, [r4]
+        ldr     r3, =CHECK_BUTTON
+        ldr     r2, [r3]
+        eor     r0, r0, r2
+        str     r0, [r3]
 
         @ Sumamos ambas máscaras
-        ldr     r4, =LED_RED_MASK
-        ldr     r5, [r4]
-        ldr     r4, =LED_GREEN_MASK
-        ldr     r6, [r4]
-        orr     r5, r5, r6
+        ldr     r3, =LED_RED_MASK
+        ldr     r0, [r3]
+        ldr     r3, =LED_GREEN_MASK
+        ldr     r1, [r3]
+        orr     r0, r0, r1
 
         @ Permutamos el valor de LED_ON
-        ldr     r4, =LED_ON
-        ldr     r6, [r4]
-        eor     r5, r5, r6
-        str     r5, [r4]
+        ldr     r3, =LED_ON
+        ldr     r2, [r3]
+        eor     r0, r0, r2
+        str     r0, [r3]
 
         @ Retornamos a donde se invocó la función
         mov     pc, lr
