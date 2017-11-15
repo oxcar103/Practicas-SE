ccac61e60b98f1ea2a0689763a43755117b2cfc0
oxcar103 <oxcar103@gmail.com>
2017-11-15 01:46:50 +0100

    Cambios menores Tabulaciones por espacios

--- bsp/hal/excep.c
+++ bsp/hal/excep.c
@@ -19,7 +19,7 @@ extern volatile excep_handler_t _excep_handlers[excep_max];
  */
 void excep_init ()
 {
-	excep_set_handler (excep_irq, excep_nonnested_irq_handler);
+    excep_set_handler (excep_irq, excep_nonnested_irq_handler);
 }
 
 /*****************************************************************************/
@@ -29,15 +29,15 @@ void excep_init ()
  * Esta función sólo funciona en modos privilegiados. Desde modo USER no se
  * permite alterar los bits I y F de los registros de control
  * @return	El valor de los bits I y F antes de deshabilitar las interrupciones:
- * 			0: I=0, F=0	(IRQ habilitadas,    FIQ habilitadas)
- * 			1: I=0, F=1	(IRQ habilitadas,    FIQ deshabilitadas)
- * 			2: I=1, F=0	(IRQ deshabilitadas, FIQ habilitadas)
- * 			3: I=1, F=1	(IRQ deshabilitadas, FIQ deshabilitadas)
+ *          0: I=0, F=0 (IRQ habilitadas,    FIQ habilitadas)
+ *          1: I=0, F=1 (IRQ habilitadas,    FIQ deshabilitadas)
+ *          2: I=1, F=0 (IRQ deshabilitadas, FIQ habilitadas)
+ *          3: I=1, F=1 (IRQ deshabilitadas, FIQ deshabilitadas)
  */
 inline uint32_t excep_disable_ints ()
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
-	return 0;
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    return 0;
 }
 
 /*****************************************************************************/
@@ -47,13 +47,13 @@ inline uint32_t excep_disable_ints ()
  * Esta función sólo funciona en modos privilegiados. Desde modo USER no se
  * permite alterar el bit I de los registros de control
  * @return	El valor del bit I antes de deshabilitar las interrupciones:
- * 			0: I=0	(IRQ habilitadas)
- * 			1: I=1	(IRQ deshabilitadas)
+ *          0: I=0  (IRQ habilitadas)
+ *          1: I=1  (IRQ deshabilitadas)
  */
 inline uint32_t excep_disable_irq ()
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
-	return 0;
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    return 0;
 }
 
 /*****************************************************************************/
@@ -63,13 +63,13 @@ inline uint32_t excep_disable_irq ()
  * Esta función sólo funciona en modos privilegiados. Desde modo USER no se
  * permite alterar el bit F de los registros de control
  * @return	El valor del bit F antes de deshabilitar las interrupciones:
- * 			0: F=0	(FIQ habilitadas)
- * 			1: F=1	(FIQ deshabilitadas)
+ *          0: F=0  (FIQ habilitadas)
+ *          1: F=1  (FIQ deshabilitadas)
  */
 inline uint32_t excep_disable_fiq ()
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
-	return 0;
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    return 0;
 }
 
 /*****************************************************************************/
@@ -78,15 +78,15 @@ inline uint32_t excep_disable_fiq ()
  * Restaura los antiguos valores de las máscaras de interrupción
  * Esta función sólo funciona en modos privilegiados. Desde modo USER no se
  * permite alterar los bits I y F de los registros de control
- * @param if_bits	Valores anteriores de las máscaras
- * 						0: I=0, F=0	(IRQ habilitadas,    FIQ habilitadas)
- *			 			1: I=0, F=1	(IRQ habilitadas,    FIQ deshabilitadas)
- * 						2: I=1, F=0	(IRQ deshabilitadas, FIQ habilitadas)
- *			 			3: I=1, F=1	(IRQ deshabilitadas, FIQ deshabilitadas)
+ * @param if_bits   Valores anteriores de las máscaras
+ *                      0: I=0, F=0 (IRQ habilitadas,    FIQ habilitadas)
+ *	                    1: I=0, F=1 (IRQ habilitadas,    FIQ deshabilitadas)
+ *                      2: I=1, F=0 (IRQ deshabilitadas, FIQ habilitadas)
+ *			 			3: I=1, F=1 (IRQ deshabilitadas, FIQ deshabilitadas)
  */
 inline void excep_restore_ints (uint32_t if_bits)
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
 }
 
 /*****************************************************************************/
@@ -95,13 +95,13 @@ inline void excep_restore_ints (uint32_t if_bits)
  * Restaura el antiguo valor de la máscara de interrupciones normales
  * Esta función sólo funciona en modos privilegiados. Desde modo USER no se
  * permite alterar el bit I de los registros de control
- * @param i_bit	Valor anterior de la máscara
- * 						0: I=0	(IRQ habilitadas)
- * 						1: I=1	(IRQ deshabilitadas)
+ * @param i_bit     Valor anterior de la máscara
+ *                      0: I=0  (IRQ habilitadas)
+ *                      1: I=1  (IRQ deshabilitadas)
  */
 inline void excep_restore_irq (uint32_t i_bit)
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
 }
 
 /*****************************************************************************/
@@ -110,37 +110,37 @@ inline void excep_restore_irq (uint32_t i_bit)
  * Restaura el antiguo valor de la máscara de interrupciones rápidas
  * Esta función sólo funciona en modos privilegiados. Desde modo USER no se
  * permite alterar el bit F de los registros de control
- * @param f_bit	Valor anterior de la máscara
- * 						0: F=0	(FIQ habilitadas)
- * 						1: F=1	(FIQ deshabilitadas)
+ * @param f_bit     Valor anterior de la máscara
+ *                      0: F=0  (FIQ habilitadas)
+ *                      1: F=1  (FIQ deshabilitadas)
  */
 inline void excep_restore_fiq (uint32_t f_bit)
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
 }
 
 /*****************************************************************************/
 
 /**
  * Asigna un manejador de interrupción/excepción
- * @param excep		Tipo de excepción
- * @param handler	Manejador
+ * @param excep     Tipo de excepción
+ * @param handler   Manejador
  */
 inline void excep_set_handler (excep_t excep, excep_handler_t handler)
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
 }
 
 /*****************************************************************************/
 
 /**
  * Retorna un manejador de interrupción/excepción
- * @param excep		Tipo de excepción
+ * @param excep     Tipo de excepción
  */
 inline excep_handler_t excep_get_handler (excep_t excep)
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
-        return NULL;
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 5 */
+    return NULL;
 }
 
 /*****************************************************************************/
@@ -154,7 +154,7 @@ inline excep_handler_t excep_get_handler (excep_t excep)
  */
 void excep_nonnested_irq_handler ()
 {
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 6 */
 }
 
 /*****************************************************************************/
