e71df45e4581e73f21d5d807873afa269d7575c0
oxcar103 <oxcar103@gmail.com>
2017-12-05 14:20:02 +0100

    Tabulaciones por espacios y llaves de inicio de función

--- bsp/drivers/uart.c
+++ bsp/drivers/uart.c
@@ -14,9 +14,8 @@
  * Acceso estructurado a los registros de control de las uart del MC1322x
  */
 
-typedef struct
-{
-	/* ESTA ESTRUCTURA SE DEFINIRÁ EN LA PRÁCTICA 8 */
+typedef struct{
+    /* ESTA ESTRUCTURA SE DEFINIRÁ EN LA PRÁCTICA 8 */
 } uart_regs_t;
 
 /*****************************************************************************/
@@ -24,9 +23,8 @@ typedef struct
 /**
  * Acceso estructurado a los pines de las uart del MC1322x
  */
-typedef struct
-{
-	gpio_pin_t tx,rx,cts,rts;
+typedef struct{
+    gpio_pin_t tx,rx,cts,rts;
 } uart_pins_t;
 
 /*****************************************************************************/
@@ -37,8 +35,8 @@ typedef struct
 static volatile uart_regs_t* const uart_regs[uart_max] = {UART1_BASE, UART2_BASE};
 
 static const uart_pins_t uart_pins[uart_max] = {
-		{gpio_pin_14, gpio_pin_15, gpio_pin_16, gpio_pin_17},
-		{gpio_pin_18, gpio_pin_19, gpio_pin_20, gpio_pin_21} };
+        {gpio_pin_14, gpio_pin_15, gpio_pin_16, gpio_pin_17},
+        {gpio_pin_18, gpio_pin_19, gpio_pin_20, gpio_pin_21} };
 
 static void uart_1_isr (void);
 static void uart_2_isr (void);
@@ -63,10 +61,9 @@ static volatile circular_buffer_t uart_circular_tx_buffers[uart_max];
 /**
  * Gestión de las callbacks
  */
-typedef struct
-{
-	uart_callback_t tx_callback;
-	uart_callback_t rx_callback;
+typedef struct{
+    uart_callback_t tx_callback;
+    uart_callback_t rx_callback;
 } uart_callbacks_t;
 
 static volatile uart_callbacks_t uart_callbacks[uart_max];
@@ -75,17 +72,16 @@ static volatile uart_callbacks_t uart_callbacks[uart_max];
 
 /**
  * Inicializa una uart
- * @param uart	Identificador de la uart
- * @param br	Baudrate
- * @param name	Nombre del dispositivo
- * @return		Cero en caso de éxito o -1 en caso de error.
- * 				La condición de error se indica en la variable global errno
+ * @param uart  Identificador de la uart
+ * @param br    Baudrate
+ * @param name  Nombre del dispositivo
+ * @return      Cero en caso de éxito o -1 en caso de error.
+ *              La condición de error se indica en la variable global errno
  */
-int32_t uart_init (uart_id_t uart, uint32_t br, const char *name)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LAS PRÁCTICAS 8, 9 y 10 */
+int32_t uart_init (uart_id_t uart, uint32_t br, const char *name){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LAS PRÁCTICAS 8, 9 y 10 */
 
-	return 0;
+    return 0;
 }
 
 /*****************************************************************************/
@@ -93,12 +89,11 @@ int32_t uart_init (uart_id_t uart, uint32_t br, const char *name)
 /**
  * Transmite un byte por la uart
  * Implementación del driver de nivel 0. La llamada se bloquea hasta que transmite el byte
- * @param uart	Identificador de la uart
- * @param c		El carácter
+ * @param uart  Identificador de la uart
+ * @param c     El carácter
  */
-void uart_send_byte (uart_id_t uart, uint8_t c)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 8 */
+void uart_send_byte (uart_id_t uart, uint8_t c){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 8 */
 }
 
 /*****************************************************************************/
@@ -106,13 +101,12 @@ void uart_send_byte (uart_id_t uart, uint8_t c)
 /**
  * Recibe un byte por la uart
  * Implementación del driver de nivel 0. La llamada se bloquea hasta que recibe el byte
- * @param uart	Identificador de la uart
- * @return		El byte recibido
+ * @param uart  Identificador de la uart
+ * @return      El byte recibido
  */
-uint8_t uart_receive_byte (uart_id_t uart)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 8 */
-        return 0;
+uint8_t uart_receive_byte (uart_id_t uart){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 8 */
+    return 0;
 }
 
 /*****************************************************************************/
@@ -120,17 +114,15 @@ uint8_t uart_receive_byte (uart_id_t uart)
 /**
  * Transmisión de bytes
  * Implementación del driver de nivel 1. La llamada es no bloqueante y se realiza mediante interrupciones
- * @param uart	Identificador de la uart
- * @param buf	Búfer con los caracteres
- * @param count	Número de caracteres a escribir
- * @return	El número de bytes almacenados en el búfer de transmisión en caso de éxito o
- *              -1 en caso de error.
- * 		La condición de error se indica en la variable global errno
+ * @param uart  Identificador de la uart
+ * @param buf   Búfer con los caracteres
+ * @param count Número de caracteres a escribir
+ * @return      El número de bytes almacenados en el búfer de transmisión en caso de éxito o -1 en caso de error.
+ *              La condición de error se indica en la variable global errno
  */
-ssize_t uart_send (uint32_t uart, char *buf, size_t count)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
-        return count;
+ssize_t uart_send (uint32_t uart, char *buf, size_t count){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
+    return count;
 }
 
 /*****************************************************************************/
@@ -138,47 +130,43 @@ ssize_t uart_send (uint32_t uart, char *buf, size_t count)
 /**
  * Recepción de bytes
  * Implementación del driver de nivel 1. La llamada es no bloqueante y se realiza mediante interrupciones
- * @param uart	Identificador de la uart
- * @param buf	Búfer para almacenar los bytes
- * @param count	Número de bytes a leer
- * @return	El número de bytes realmente leídos en caso de éxito o
- *              -1 en caso de error.
- * 		La condición de error se indica en la variable global errno
+ * @param uart  Identificador de la uart
+ * @param buf   Búfer para almacenar los bytes
+ * @param count Número de bytes a leer
+ * @return      El número de bytes realmente leídos en caso de éxito o -1 en caso de error.
+ *              La condición de error se indica en la variable global errno
  */
-ssize_t uart_receive (uint32_t uart, char *buf, size_t count)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
-        return 0;
+ssize_t uart_receive (uint32_t uart, char *buf, size_t count){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
+    return 0;
 }
 
 /*****************************************************************************/
 
 /**
  * Fija la función callback de recepción de una uart
- * @param uart	Identificador de la uart
- * @param func	Función callback. NULL para anular una selección anterior
- * @return	Cero en caso de éxito o -1 en caso de error.
- * 		La condición de error se indica en la variable global errno
+ * @param uart  Identificador de la uart
+ * @param func  Función callback. NULL para anular una selección anterior
+ * @return      Cero en caso de éxito o -1 en caso de error.
+ *              La condición de error se indica en la variable global errno
  */
-int32_t uart_set_receive_callback (uart_id_t uart, uart_callback_t func)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
-        return 0;
+int32_t uart_set_receive_callback (uart_id_t uart, uart_callback_t func){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
+    return 0;
 }
 
 /*****************************************************************************/
 
 /**
  * Fija la función callback de transmisión de una uart
- * @param uart	Identificador de la uart
- * @param func	Función callback. NULL para anular una selección anterior
- * @return	Cero en caso de éxito o -1 en caso de error.
- * 		La condición de error se indica en la variable global errno
+ * @param uart  Identificador de la uart
+ * @param func  Función callback. NULL para anular una selección anterior
+ * @return      Cero en caso de éxito o -1 en caso de error.
+ *              La condición de error se indica en la variable global errno
  */
-int32_t uart_set_send_callback (uart_id_t uart, uart_callback_t func)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
-        return 0;
+int32_t uart_set_send_callback (uart_id_t uart, uart_callback_t func){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
+    return 0;
 }
 
 /*****************************************************************************/
@@ -188,11 +176,10 @@ int32_t uart_set_send_callback (uart_id_t uart, uart_callback_t func)
  * Cada isr llamará a este manejador indicando la uart en la que se ha
  * producido la interrupción.
  * Lo declaramos inline para reducir la latencia de la isr
- * @param uart	Identificador de la uart
+ * @param uart  Identificador de la uart
  */
-static inline void uart_isr (uart_id_t uart)
-{
-	/* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
+static inline void uart_isr (uart_id_t uart){
+    /* ESTA FUNCIÓN SE DEFINIRÁ EN LA PRÁCTICA 9 */
 }
 
 /*****************************************************************************/
@@ -200,9 +187,8 @@ static inline void uart_isr (uart_id_t uart)
 /**
  * Manejador de interrupciones para la uart1
  */
-static void uart_1_isr (void)
-{
-	uart_isr(uart_1);
+static void uart_1_isr (void){
+    uart_isr(uart_1);
 }
 
 /*****************************************************************************/
@@ -210,9 +196,8 @@ static void uart_1_isr (void)
 /**
  * Manejador de interrupciones para la uart2
  */
-static void uart_2_isr (void)
-{
-	uart_isr(uart_2);
+static void uart_2_isr (void){
+    uart_isr(uart_2);
 }
 
 /*****************************************************************************/
