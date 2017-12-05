c596ef622332dc7252bcff82796497e5185b550a
oxcar103 <oxcar103@gmail.com>
2017-12-05 15:54:12 +0100

    Limpieza de los comentarios

--- bsp/include/uart.h
+++ bsp/include/uart.h
@@ -34,7 +34,8 @@ typedef void (* uart_callback_t) (void);
  * @param uart  Identificador de la uart
  * @param br    Baudrate
  * @param name  Nombre del dispositivo
- * @return      Cero en caso de éxito o -1 en caso de error.
+ * @return      0 en caso de éxito
+                -1 en caso de error
  *              La condición de error se indica en la variable global errno
  */
 int32_t uart_init (uart_id_t uart, uint32_t br, const char *name);
@@ -43,7 +44,8 @@ int32_t uart_init (uart_id_t uart, uint32_t br, const char *name);
 
 /**
  * Transmite un byte por la uart
- * Implementación del driver de nivel 0. La llamada se bloquea hasta que transmite el byte
+ * Implementación del driver de nivel 0
+ * La llamada se bloquea hasta que transmite el byte
  * @param uart  Identificador de la uart
  * @param c     El carácter
  */
@@ -53,7 +55,8 @@ void uart_send_byte (uart_id_t uart, uint8_t c);
 
 /**
  * Recibe un byte por la uart
- * Implementación del driver de nivel 0. La llamada se bloquea hasta que recibe el byte
+ * Implementación del driver de nivel 0
+ * La llamada se bloquea hasta que recibe el byte
  * @param uart  Identificador de la uart
  * @return      El byte recibido
  */
@@ -63,11 +66,13 @@ uint8_t uart_receive_byte (uart_id_t uart);
 
 /**
  * Transmisión de bytes
- * Implementación del driver de nivel 1. La llamada es no bloqueante y se realiza mediante interrupciones
+ * Implementación del driver de nivel 1
+ * La llamada es no bloqueante y se realiza mediante interrupciones
  * @param uart  Identificador de la uart
  * @param buf   Búfer con los caracteres
  * @param count Número de caracteres a escribir
- * @return      El número de bytes almacenados en el búfer de transmisión en caso de éxito o -1 en caso de error.
+ * @return      Número de bytes almacenados en caso de éxito
+ *              -1 en caso de error
  *              La condición de error se indica en la variable global errno
  */
 ssize_t uart_send (uint32_t uart, char *buf, size_t count);
@@ -76,11 +81,13 @@ ssize_t uart_send (uint32_t uart, char *buf, size_t count);
 
 /**
  * Recepción de bytes
- * Implementación del driver de nivel 1. La llamada es no bloqueante y se realiza mediante interrupciones
+ * Implementación del driver de nivel 1
+ * La llamada es no bloqueante y se realiza mediante interrupciones
  * @param uart  Identificador de la uart
  * @param buf   Búfer para almacenar los bytes
  * @param count Número de bytes a leer
- * @return      El número de bytes realmente leídos en caso de éxito o -1 en caso de error.
+ * @return      Número de bytes leídos en caso de éxito
+ *              -1 en caso de error
  *              La condición de error se indica en la variable global errno
  */
 ssize_t uart_receive (uint32_t uart, char *buf, size_t count);
@@ -91,7 +98,8 @@ ssize_t uart_receive (uint32_t uart, char *buf, size_t count);
  * Fija la función callback de recepción de una uart
  * @param uart  Identificador de la uart
  * @param func  Función callback. NULL para anular una selección anterior
- * @return      Cero en caso de éxito o -1 en caso de error.
+ * @return      0 en caso de éxito
+ *              -1 en caso de error
  *              La condición de error se indica en la variable global errno
  */
 int32_t uart_set_receive_callback (uart_id_t uart, uart_callback_t func);
@@ -102,7 +110,8 @@ int32_t uart_set_receive_callback (uart_id_t uart, uart_callback_t func);
  * Fija la función callback de transmisión de una uart
  * @param uart  Identificador de la uart
  * @param func  Función callback. NULL para anular una selección anterior
- * @return      Cero en caso de éxito o -1 en caso de error.
+ * @return      0 en caso de éxito
+ *              -1 en caso de error
  *              La condición de error se indica en la variable global errno
  */
 int32_t uart_set_send_callback (uart_id_t uart, uart_callback_t func);
