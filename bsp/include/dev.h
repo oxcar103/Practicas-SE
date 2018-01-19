83baff0c6d1aafcf00f776f86376df242ea960ae
oxcar103 <oxcar103@gmail.com>
2018-01-20 00:27:42 +0100

    Más espacios y llaves

--- bsp/include/dev.h
+++ bsp/include/dev.h
@@ -14,20 +14,19 @@
 /**
  * Estructura para almacenar las funciones de gestión de cada dispositivo
  */
-typedef struct
-{
-	const char  *name;										/* Nombre del dispositivo */
-	uint32_t id;											/* Identificador del dispositivo */
-															/* Por defecto es cero. Se usa para */
-															/* diferenciar a varios dispositivos */
-															/* del mismo tipo */
-	int (*open)(uint32_t id, int flags, mode_t mode);		/* Función open */
-	int (*close)(uint32_t id);								/* Función close */
-	ssize_t (*read)(uint32_t id, char *buf, size_t count);	/* Función read */
-	ssize_t (*write)(uint32_t id, char *buf, size_t count);	/* Función write */
-	off_t (*lseek)(uint32_t id, off_t offset, int whence);	/* Función lseek */
-	int (*fstat)(uint32_t id, struct stat *buf);			/* Función fstat */
-	int (*isatty)(uint32_t id);								/* Función isatty */
+typedef struct{
+    const char  *name;                                      /* Nombre del dispositivo */
+    uint32_t id;                                            /* Identificador del dispositivo */
+                                                            /* Por defecto es cero. Se usa para */
+                                                            /* diferenciar a varios dispositivos */
+                                                            /* del mismo tipo */
+    int (*open)(uint32_t id, int flags, mode_t mode);       /* Función open */
+    int (*close)(uint32_t id);                              /* Función close */
+    ssize_t (*read)(uint32_t id, char *buf, size_t count);  /* Función read */
+    ssize_t (*write)(uint32_t id, char *buf, size_t count); /* Función write */
+    off_t (*lseek)(uint32_t id, off_t offset, int whence);  /* Función lseek */
+    int (*fstat)(uint32_t id, struct stat *buf);            /* Función fstat */
+    int (*isatty)(uint32_t id);                             /* Función isatty */
 } bsp_dev_t;
 
 /*****************************************************************************/
@@ -35,26 +34,25 @@ typedef struct
 /**
  * Estructura de un descriptor de fichero
  */
-typedef struct
-{
-	bsp_dev_t* dev;        /* Puntero a la estructura gestión del dispositivo */
-	int        flags;      /* Flags de apertura/creación del fichero */
+typedef struct{
+    bsp_dev_t*  dev;    /* Puntero a la estructura gestión del dispositivo */
+    int         flags;  /* Flags de apertura/creación del fichero */
 } bsp_fd_t;
 
 /*****************************************************************************/
 
 /**
  * Registro de un dispositivo en el sistema.
- * @param name		Nombre del dispositivo
- * @param id		Identificador del dispositivo
- * @param open		Función de apertura del dispositivo
- * @param close		Función close del dispositivo
- * @param read		Función read del dispositivo
- * @param write		Función write del dispositivo
- * @param lseek		Función lseek del dispositivo
- * @param fstat		Función fsat del dispositivo
- * @param isatty	Función isatty del dispositivo
- * @return 			El numero de dispositivo asignado o -1 en caso de error
+ * @param name      Nombre del dispositivo
+ * @param id        Identificador del dispositivo
+ * @param open      Función de apertura del dispositivo
+ * @param close     Función close del dispositivo
+ * @param read      Función read del dispositivo
+ * @param write     Función write del dispositivo
+ * @param lseek     Función lseek del dispositivo
+ * @param fstat     Función fsat del dispositivo
+ * @param isatty    Función isatty del dispositivo
+ * @return          El numero de dispositivo asignado o -1 en caso de error
  */
 int32_t bsp_register_dev (const char  *name,
 		uint32_t id,
@@ -70,7 +68,7 @@ int32_t bsp_register_dev (const char  *name,
 
 /**
  * Busca un dispositivo en el sistema
- * @param pathname   Nombre del dispositivo
+ * @param pathname  Nombre del dispositivo
  */
 bsp_dev_t * find_dev (const char *pathname);
 
@@ -78,7 +76,7 @@ bsp_dev_t * find_dev (const char *pathname);
 
 /**
  * Retorna el puntero del dispositivo asociado al descriptor de un fichero
- * @param fd   El descriptor
+ * @param fd    El descriptor
  */
 inline bsp_dev_t* get_dev (uint32_t fd);
 
@@ -86,7 +84,7 @@ inline bsp_dev_t* get_dev (uint32_t fd);
 
 /**
  * Retorna los flags de apertura de un fichero
- * @param fd   El descriptor
+ * @param fd    El descriptor
  */
 inline int get_flags (uint32_t fd);
 
@@ -94,10 +92,10 @@ inline int get_flags (uint32_t fd);
 
 /**
  * Asigna un nuevo descriptor de fichero a un dispositivo
- * @param dev	El dispositivo
- * @param flags	Modo de acceso seleccionado en su apertura
- * @return 		El numero de descriptor o -1 en caso de error. La condición de error
- * 				se indica en la variable global errno.
+ * @param dev   El dispositivo
+ * @param flags Modo de acceso seleccionado en su apertura
+ * @return      El numero de descriptor o -1 en caso de error. La condición de error
+ *              se indica en la variable global errno.
  */
 int32_t get_fd(bsp_dev_t *dev, int flags);
 
